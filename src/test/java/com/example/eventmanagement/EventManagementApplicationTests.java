8d69b1f8ec8e27f38f376e6a0998334874ea95b4
Deem0n564 <dimas564564@gmail.com>
2026-04-30 08:51:03 +0300

    lab7.0.1

--- src/test/java/com/example/eventmanagement/EventManagementApplicationTests.java
+++ src/test/java/com/example/eventmanagement/EventManagementApplicationTests.java
@@ -2,12 +2,22 @@ package com.example.eventmanagement;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class EventManagementApplicationTests {
 
+    private final ApplicationContext applicationContext;
+
+    EventManagementApplicationTests(ApplicationContext applicationContext) {
+        this.applicationContext = applicationContext;
+    }
+
     @Test
     void contextLoads() {
+        assertNotNull(applicationContext);
     }
 
 }
